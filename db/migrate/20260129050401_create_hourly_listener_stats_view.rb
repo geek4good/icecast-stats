314e2bdddabd881e2fb7f649fcf6bf80591b73e6
Lucas Mbiwe <lucas@geek4good.com>
2026-02-02 18:54:32 +0700

    Make the hourly stats view more robust

--- db/migrate/20260129050401_create_hourly_listener_stats_view.rb
+++ db/migrate/20260129050401_create_hourly_listener_stats_view.rb
@@ -6,13 +6,15 @@ class CreateHourlyListenerStatsView < ActiveRecord::Migration[8.1]
         date(snapshots.created_at) as date,
         CAST(strftime('%H', snapshots.created_at) AS INTEGER) as hour,
         json_extract(stream_data.value, '$.server_name') as stream_name,
-        AVG(CAST(json_extract(stream_data.value, '$.listeners') AS INTEGER)) as avg_listeners,
+        CAST(ROUND(AVG(json_extract(stream_data.value, '$.listeners'))) AS INTEGER) as avg_listeners,
         MAX(CAST(json_extract(stream_data.value, '$.listeners') AS INTEGER)) as max_listeners,
         COUNT(*) as snapshot_count
       FROM snapshots
       CROSS JOIN json_each(
         json_extract(snapshots.stats, '$.icestats.source')
       ) as stream_data
+      WHERE json_valid(snapshots.stats) > 0
+      AND json_extract(snapshots.stats, '$.icestats.source[0]') NOT NULL
       GROUP BY date, hour, stream_name
       ORDER BY date, hour, stream_name;
     SQL
