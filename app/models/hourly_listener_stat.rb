1be2bb334176544261e6109f78f008aa5df91ad7
Lucas Mbiwe <lucas@geek4good.com>
2026-02-25 19:49:57 +0700

    Straighten out database schema
    Add 'station' column to the 'listener_stats' table and the index. This
    is possible, because it hasn't been deployed on the server yet, so the
    migration has never run there.
    
    The 'hourly_listener_stats' view doesn't make much sense, since it's not
    a materialised view. So, simply generating and using 'listener_stats' is
    much more performant.

--- app/models/hourly_listener_stat.rb
+++
@@ -1,9 +0,0 @@
-class HourlyListenerStat < ApplicationRecord
-  scope :surf_radio, -> { where(stream_name: "Surf Radio") }
-  scope :talay_fm, -> { where(stream_name: "Talay FM") }
-
-  def self.on(date)
-    where(date:)
-  end
-end
-
--- db/migrate/20251212052132_create_listener_stats.rb
+++ db/migrate/20251212052132_create_listener_stats.rb
@@ -1,6 +1,7 @@
 class CreateListenerStats < ActiveRecord::Migration[8.1]
   def change
     create_table :listener_stats do |t|
+      t.text :station
       t.datetime :from
       t.datetime :to
       t.integer :average
@@ -11,6 +12,6 @@ class CreateListenerStats < ActiveRecord::Migration[8.1]
       t.timestamps
     end
 
-    add_index :listener_stats, [:from, :to], unique: true
+    add_index :listener_stats, [:station, :from, :to], unique: true
   end
 end
