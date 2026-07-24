92134038d9c3fa9f04b33c8d67a7c7cbbb0134b1
Lucas Mbiwe <lucas@geek4good.com>
2026-07-24 14:17:27 +0700

    refactor: simplify song play extraction
    Compute yesterday's range more directly from beginning_of_day instead
    of subtracting a day first.

--- app/jobs/song_play_extraction_job.rb
+++ app/jobs/song_play_extraction_job.rb
@@ -2,9 +2,8 @@ class SongPlayExtractionJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    yesterday = Time.current.utc.beginning_of_day - 1.day
-    from = yesterday
-    to = yesterday + 1.day
+    to = Time.current.utc.beginning_of_day
+    from = to - 1.day
     SongPlayExtractor.new(from: from, to: to).extract
   end
 end
